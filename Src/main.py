4308f67766c773064f8b65cab18fb5b5d8856788
sanjay-devz <sanjay.gridz@gmail.com>
2026-09-13 21:55:39 +0530

    import errors

--- Src/main.py
+++ Src/main.py
@@ -3,8 +3,8 @@ from text_chunker import chunk_text
 from embedder import create_embeddings
 from vector_store import create_vector_store
 from prompt_builder import build_prompt
-from retriever import retrieve
-from numpy import pi
+from retriever import 
+from numpy import 
 
 pdf_path = "50_Gen_AI_Product_Ideas_E2E.pdf"
 
